f03bd59bada799d2ba54d7dcfe3cc6db2cd2bd5d
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-21 23:12:02 +0300

    Completed the dom editing lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const remove = document.querySelector ('#main')
+main .remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Rose is the champion'
+
+
